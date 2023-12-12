b2997522db30d932705ebd648b51a50562de7e9d
Jeff <jeff_lo@smartone.com>
2023-12-12 17:55:16 +0800

    christmas_banner_v2

--- src/working_files/JS_V3/storefront/components/banner/christmas.js
+++ src/working_files/JS_V3/storefront/components/banner/christmas.js
@@ -16,11 +16,11 @@ import {
   CONTENT.TC.DEFAULT_PLAN_URL = `/tc/storefront/christmas-gifting-catalog_2023.jsp?itm_campaign=20231214-christmas-os-popupbanner&itm_source=smartone-onlinestore&itm_medium=banner&itm_content=tc&itm_ga=os`;
   CONTENT.EN.DEFAULT_PLAN_URL = `/en/storefront/christmas-gifting-catalog_2023.jsp?itm_campaign=20231214-christmas-os-popupbanner&itm_source=smartone-onlinestore&itm_medium=banner&itm_content=en&itm_ga=os`;
   
-  CONTENT.TC.BG = 'IMG_V4/popup_banner/christmas/tc/Christmas_Campaign_PageBanner_V1_d_TC.jpg';
-  CONTENT.TC.BG_M = 'IMG_V4/popup_banner/christmas/tc/Christmas_Campaign_PageBanner_V1_m_TC.jpg';
+  CONTENT.TC.BG = 'IMG_V4/popup_banner/christmas/tc/Christmas_pop-up_banner_d_V2_TC.jpg';
+  CONTENT.TC.BG_M = 'IMG_V4/popup_banner/christmas/tc/Christmas_pop-up_banner_m_V2_TC.jpg';
   
-  CONTENT.EN.BG = 'IMG_V4/popup_banner/christmas/en/Christmas_Campaign_PageBanner_V1_d_EN.jpg';
-  CONTENT.EN.BG_M = 'IMG_V4/popup_banner/christmas/en/Christmas_Campaign_PageBanner_V1_m_EN.jpg';
+  CONTENT.EN.BG = 'IMG_V4/popup_banner/christmas/en/Christmas_pop-up_banner_d_V2_EN.jpg';
+  CONTENT.EN.BG_M = 'IMG_V4/popup_banner/christmas/en/Christmas_pop-up_banner_m_V2_EN.jpg';
   
   const christmas = POPUP_BANNER.init({
 	content: CONTENT,
