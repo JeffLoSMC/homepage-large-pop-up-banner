3d70e58c091000e9f1ebe745c344ab02c6c27650
Jeff <jeff_lo@smartone.com>
2024-02-01 15:13:18 +0800

    u_cnyRewards_banner

--- src/working_files/JS_V3/storefront/components/banner/cnyRewards.js
+++ src/working_files/JS_V3/storefront/components/banner/cnyRewards.js
@@ -13,8 +13,8 @@ import {
   CONTENT.TC.DEFAULT_TERMS = ``;
   CONTENT.EN.DEFAULT_TERMS = ``;
   
-  CONTENT.TC.DEFAULT_PLAN_URL = `/tc/storefront/samsung-cny-rewards-week_2024.jsp`;
-  CONTENT.EN.DEFAULT_PLAN_URL = `/en/storefront/samsung-cny-rewards-week_2024.jsp`;
+  CONTENT.TC.DEFAULT_PLAN_URL = `/tc/storefront/samsung-cny-rewards-week_2024.jsp?itm_campaign=20240201-cny-samsung-os-pop&itm_source=smartone-onlinestore&itm_medium=banner&itm_content=tc&itm_ga=os`;
+  CONTENT.EN.DEFAULT_PLAN_URL = `/en/storefront/samsung-cny-rewards-week_2024.jsp?itm_campaign=20240201-cny-samsung-os-pop&itm_source=smartone-onlinestore&itm_medium=banner&itm_content=en&itm_ga=os`;
 
   CONTENT.TC.BG = 'IMG_V4/popup_banner/CNY_Vday_samsung/tc/CNY_Vday_samsung_banner_d.jpg';
   CONTENT.TC.BG_M = 'IMG_V4/popup_banner/CNY_Vday_samsung/tc/CNY_Vday_samsung_banner_m.jpg';
