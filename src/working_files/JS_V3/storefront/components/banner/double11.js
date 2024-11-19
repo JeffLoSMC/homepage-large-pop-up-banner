56c5bd421b921557746d85809dfbc8116ca63585
Jeff <jeff_lo@smartone.com>
2024-11-19 17:38:54 +0800

    double11 images update

--- src/working_files/JS_V3/storefront/components/banner/double11.js
+++ src/working_files/JS_V3/storefront/components/banner/double11.js
@@ -16,11 +16,11 @@ import {
   CONTENT.TC.DEFAULT_PLAN_URL = "/tc/storefront/double11_2024.jsp?itm_campaign=20241101-1111offer&itm_source=itm&itm_medium=pop-up-banner&itm_content=OStore-popup-tc&itm_ga=os";
   CONTENT.EN.DEFAULT_PLAN_URL = "/en/storefront/double11_2024.jsp?itm_campaign=20241101-1111offer&itm_source=itm&itm_medium=pop-up-banner&itm_content=OStore-popup-en&itm_ga=os";
   
-  CONTENT.TC.BG = 'IMG_V4/popup_banner/D11_Campaign_popup/D11_Campaign_popupTC.jpg';
-  CONTENT.TC.BG_M = 'IMG_V4/popup_banner/D11_Campaign_popup/D11_Campaign_popup_mobTC.jpg';
+  CONTENT.TC.BG = 'IMG_V4/popup_banner/D11_Campaign_popup_lastcall/D11_Campaign_popup_lastcall_TC.jpg';
+  CONTENT.TC.BG_M = 'IMG_V4/popup_banner/D11_Campaign_popup_lastcall/D11_Campaign_popup_lastcall_mobTC.jpg';
   
-  CONTENT.EN.BG = 'IMG_V4/popup_banner/D11_Campaign_popup/D11_Campaign_popupEN.jpg';
-  CONTENT.EN.BG_M = 'IMG_V4/popup_banner/D11_Campaign_popup/D11_Campaign_popup_mobEN.jpg';
+  CONTENT.EN.BG = 'IMG_V4/popup_banner/D11_Campaign_popup_lastcall/D11_Campaign_popup_lastcall_EN.jpg';
+  CONTENT.EN.BG_M = 'IMG_V4/popup_banner/D11_Campaign_popup_lastcall/D11_Campaign_popup_lastcall_mobEN.jpg';
   
   const double11 = POPUP_BANNER.init({
     content: CONTENT,
