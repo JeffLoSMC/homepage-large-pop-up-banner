7d748cb6f524fc20c2c4522962eefeaf1acf8d27
Jeff <jeff_lo@smartone.com>
2025-05-19 15:35:15 +0800

    Disabled motherday2025 popup

--- src/working_files/JS_V3/storefront/overlay_banner.js
+++ src/working_files/JS_V3/storefront/overlay_banner.js
@@ -97,52 +97,52 @@ OVERLAYBANNER.HOMEPAGE_LARGE_POPUP_BANNER = {
   }
 
 
-  OVERLAYBANNER.initBanner = (target, specific) => {
-    const _target = target || document.getElementById('site-preloader');
+  // OVERLAYBANNER.initBanner = (target, specific) => {
+  //   const _target = target || document.getElementById('site-preloader');
 
-    //if there is ONLY ONE version for the popup banner
-    const _callback = () => {
-      motherday2025({
-        rendered: () => POPUP_BANNER.addCountOfShow(1, CAMPAIGN.MOTHERDAY2025),
-      });
-    };
+  //   //if there is ONLY ONE version for the popup banner
+  //   const _callback = () => {
+  //     motherday2025({
+  //       rendered: () => POPUP_BANNER.addCountOfShow(1, CAMPAIGN.MOTHERDAY2025),
+  //     });
+  //   };
     
-    return _callback();
+  //   return _callback();
 
-    // //two banners
-    // const _callback = () => {
-    //   if(specific == "cnyWorkingHour"){
-    //     importantNotice({
-    //       rendered: () => POPUP_BANNER.addCountOfShow(1, CAMPAIGN.IMPORTANTNOTICE),
-    //     });
-    //   }else{
-    //     let selectedCampaign = twoCampaignSelect(CAMPAIGN.VDAY2025APPLE, CAMPAIGN.VDAY2025ANDROID);
-    //     if(selectedCampaign == CAMPAIGN.VDAY2025APPLE){
-    //       campaignPopupFirst({
-    //         rendered: () => POPUP_BANNER.addCountOfShow(1, CAMPAIGN.VDAY2025APPLE),
-    //       });
-    //     }
-    //     if(selectedCampaign == CAMPAIGN.VDAY2025ANDROID){
-    //       campaignPopupSecond({
-    //         rendered: () => POPUP_BANNER.addCountOfShow(1, CAMPAIGN.VDAY2025ANDROID),
-    //       });
-    //     }
-    //   }
-    // };
+  //   // //two banners
+  //   // const _callback = () => {
+  //   //   if(specific == "cnyWorkingHour"){
+  //   //     importantNotice({
+  //   //       rendered: () => POPUP_BANNER.addCountOfShow(1, CAMPAIGN.IMPORTANTNOTICE),
+  //   //     });
+  //   //   }else{
+  //   //     let selectedCampaign = twoCampaignSelect(CAMPAIGN.VDAY2025APPLE, CAMPAIGN.VDAY2025ANDROID);
+  //   //     if(selectedCampaign == CAMPAIGN.VDAY2025APPLE){
+  //   //       campaignPopupFirst({
+  //   //         rendered: () => POPUP_BANNER.addCountOfShow(1, CAMPAIGN.VDAY2025APPLE),
+  //   //       });
+  //   //     }
+  //   //     if(selectedCampaign == CAMPAIGN.VDAY2025ANDROID){
+  //   //       campaignPopupSecond({
+  //   //         rendered: () => POPUP_BANNER.addCountOfShow(1, CAMPAIGN.VDAY2025ANDROID),
+  //   //       });
+  //   //     }
+  //   //   }
+  //   // };
 
 
-    // if (POPUP_BANNER.isNotReachCountOfShow(3) || ASSIST.isDev) {
-    //   const _target = target || document.getElementById('site-preloader');
-    //   const _callback = () => {
-    //     christmas({
-    //       rendered: () => POPUP_BANNER.addCountOfShow(1),
-    //     });
-    //   };
-    //   return _callback();
-		// } else {
-    //   return;
-    // }  
-  };
+  //   // if (POPUP_BANNER.isNotReachCountOfShow(3) || ASSIST.isDev) {
+  //   //   const _target = target || document.getElementById('site-preloader');
+  //   //   const _callback = () => {
+  //   //     christmas({
+  //   //       rendered: () => POPUP_BANNER.addCountOfShow(1),
+  //   //     });
+  //   //   };
+  //   //   return _callback();
+	// 	// } else {
+  //   //   return;
+  //   // }  
+  // };
   
 
   // '?banner={{initBannerOn}} e.g.?overlayBanner=mnp-offer-check'
